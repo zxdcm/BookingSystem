a07cab5d4d4c661a1483f80068cc16f4cb39aa88
Egor Kirilin <kirilin.egor@gmail.com>
2019-02-21 13:57:11 +0300

    get rid of string split

--- BookingSystem/BookingDatabase/Script.PostDeployment.sql
+++ BookingSystem/BookingDatabase/Script.PostDeployment.sql
@@ -82,58 +82,48 @@ VALUES
 
 
 
-DECLARE @Countries NVARCHAR(MAX);
-DECLARE @Cities NVARCHAR(MAX);
-DECLARE @Addresses NVARCHAR(MAX);
-DECLARE @HotelsNames NVARCHAR(MAX);
 
-SET @Countries = 'United States;United Kingdom;Spain';
-SET @Cities = 'New York;London;Madrid';
-SET @Addresses = '243 West 55th St., New York, NY 10019; 83-95 Southampton Row, London WC1B 4HD;Calle Gran Vía, 74 Madrid';
-SET @HotelsNames = 'Moderne Hotel;The County;Madrid Plaza';
-
-
-INSERT INTO [dbo].[Countries]([Name])  
-SELECT value FROM STRING_SPLIT(@Countries, ';');
-
+INSERT INTO [dbo].[Countries]([Name]) 
+VALUES ('United States'), ('United Kingdom'), ('Spain')
 
 INSERT INTO [dbo].[Cities]([CountryId], [Name])
 SELECT [Countries].[CountryId], [Cities].[Name]
 FROM (SELECT [CountryId], ROW_NUMBER() OVER (ORDER BY (SELECT 0)) AS row_num 
 	  FROM [dbo].[Countries]) AS [Countries]
 JOIN 
-	(SELECT VALUE AS [Name], ROW_NUMBER() OVER (ORDER BY (SELECT 0)) AS row_num 
-	 FROM STRING_SPLIT(@Cities, ';')) AS [Cities]
+	(SELECT [Name], ROW_NUMBER() OVER (ORDER BY (SELECT 0)) AS row_num
+	 FROM (VALUES ('New York'),('London'),('Madrid')) AS [Cities]([Name])) AS [Cities]
 ON [Countries].row_num = [Cities].row_num
 
 
-INSERT INTO [dbo].[Locations]([CityId], [Address])
-SELECT [Cities].[CityId], [Addresses].[Address]
-FROM (SELECT [CityId], ROW_NUMBER() OVER (ORDER BY (SELECT 0)) AS row_num 
-	  FROM [dbo].[Cities]) AS [Cities]
+INSERT INTO [dbo].[Hotels]([Name],[Address],[CountryId], [CityId])
+SELECT [Hotels].[Name], [Addresses].[Address], [Countries].CountryId, [Cities].[CityId]
+FROM (SELECT [Name], ROW_NUMBER() OVER (ORDER BY (SELECT 0)) AS row_num 
+	  FROM (VALUES ('Moderne Hotel'), ('The County'), ('Madrid Plaza')) AS [Hotels]([Name])) AS [Hotels]
 JOIN 
-	(SELECT value AS [Address], ROW_NUMBER() OVER (ORDER BY (SELECT 0)) AS row_num 
-	 FROM STRING_SPLIT(@Addresses, ';')) AS [Addresses]
-ON [Cities].row_num = [Addresses].row_num
-
-
-INSERT INTO [dbo].[Hotels]([Name],[LocationId],[CountryId])
-SELECT [Hotels].[Name], [Locations].[LocationId], [Countries].CountryId
-FROM (SELECT value AS [Name], ROW_NUMBER() OVER (ORDER BY (SELECT 0)) AS row_num 
-	  FROM STRING_SPLIT(@HotelsNames, ';')) AS [Hotels]
-JOIN 
-	(SELECT [LocationId], ROW_NUMBER() OVER (ORDER BY (SELECT 0)) AS row_num 
-	 FROM [dbo].[Locations]) AS [Locations]
-ON [Hotels].row_num = [Locations].row_num
+	(SELECT [CityId], ROW_NUMBER() OVER (ORDER BY (SELECT 0)) AS row_num 
+	 FROM [dbo].[Cities]) AS [Cities]
+ON [Hotels].row_num = [Cities].row_num
 JOIN 
 	(SELECT [CountryId], ROW_NUMBER() OVER (ORDER BY (SELECT 0)) AS row_num 
 	 FROM [dbo].[Countries]) AS [Countries]
-ON [Countries].row_num = [Locations].row_num
+ON [Countries].row_num = [Hotels].row_num
+JOIN 
+	(SELECT [Address], ROW_NUMBER() OVER (ORDER BY (SELECT 0)) AS row_num
+	 FROM 
+	 (VALUES ('243 West 55th St., New York, NY 10019'),
+		 	 ('83-95 Southampton Row, London WC1B 4HD'),
+		     ('Calle Gran Vía, 74 Madrid')) 
+	 AS [Addresses]([Address])) AS [Addresses]
+ON [Addresses].row_num = [Hotels].row_num
 
 
 DECLARE @FirstHotelId INT, @SecondHotelId INT;
 SET @FirstHotelId = (SELECT [HotelId] FROM [dbo].[Hotels] WHERE [Name] = 'Moderne Hotel');
 SET @SecondHotelId = (SELECT [HotelId] FROM [dbo].[Hotels] WHERE [Name] = 'The County');
+
+PRINT cast(@FirstHotelId as varchar);
+PRINT cast(@SecondHotelId as varchar);
 
 INSERT INTO [dbo].Rooms([HotelId], [Name], [Price], [Size])
 VALUES
@@ -166,7 +156,7 @@ DECLARE @SecondRoomNumberId INT;
 SET @FirstRoomNumberId = (SELECT [RoomNumberId] FROM [dbo].[RoomNumbers] WHERE [Number] = 1);
 SET @SecondRoomNumberId = (SELECT [RoomNumberId] FROM [dbo].[RoomNumbers] WHERE [Number] = 2);
 
-INSERT INTO [dbo].[Bookings](RoomNumberId, UserId, Created, Status)
+INSERT INTO [dbo].[Bookings](RoomNumberId, UserId, CreatedDate, Status)
 VALUES
 	(@FirstRoomNumberId, @UserId, SYSDATETIME(), 1),
 	(@SecondRoomNumberId, @UserId, SYSDATETIME(), 1);
