774498c3339db95611b91382530d15568e9d0b02
Egor Kirilin <kirilin.egor@gmail.com>
2019-03-06 14:32:38 +0300

    AddHotel & EditHotel methods now specifies resource location. Commented parts of useless code are removed. Unused imports removed.

--- BookingSystem/BookingSystem.WebApi/Controllers/HotelController.cs
+++ BookingSystem/BookingSystem.WebApi/Controllers/HotelController.cs
@@ -1,16 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 using AutoMapper;
 using BookingSystem.Commands.Commands.HotelCommands.Commands;
 using BookingSystem.Commands.Commands.HotelCommands.DTOs;
+using BookingSystem.Commands.Infrastructure;
 using BookingSystem.Common.Interfaces;
-using BookingSystem.Queries.HotelQueries;
-using BookingSystem.Queries.Views;
-using BookingSystem.WritePersistence;
-using BookingSystem.WritePersistence.WriteModels;
+using BookingSystem.Queries.Queries.HotelQueries.Queries;
+using BookingSystem.Queries.Queries.HotelQueries.Views;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -18,26 +14,18 @@ using Microsoft.Extensions.Logging;
 
 namespace BookingSystem.WebApi.Controllers
 {
-    //Todo: rm project reference later
-    //Todo: rm PropertyDescriptor
-    //Todo: rm write context reference
     [Produces("application/json")]
     [Route("api/hotels")]
     [ApiController]
     public class HotelController : BaseController
     {
-        private readonly IMapper _mapper;
-        private readonly ILogger<HotelController> _logger;
         private readonly ICommandDispatcher _commandDispatcher;
         private readonly IQueryDispatcher _queryDispatcher;
         
-        public HotelController(IMapper mapper, 
-            ILogger<HotelController> logger, 
+        public HotelController(
             IQueryDispatcher queryDispatcher, 
             ICommandDispatcher commandDispatcher)
         {
-            _mapper = mapper;
-            _logger = logger;
             _queryDispatcher = queryDispatcher;
             _commandDispatcher = commandDispatcher;
         }
@@ -54,16 +42,9 @@ namespace BookingSystem.WebApi.Controllers
         /// </remarks>
         /// <returns>List of hotels views</returns>
         [HttpGet]
-        public async Task<IEnumerable<HotelView>> GetHotelsList([FromQuery] ListHotelsQuery query)
+        public async Task<IEnumerable<HotelPreView>> GetHotelsListAsync([FromQuery] ListHotelsQuery query)
         {
             return await _queryDispatcher.Dispatch(query).ToArrayAsync();
-//            var dto = new EditedHotelDto() { CityId = 1, Address = "10", CountryId = 1, IsActive = true, Name = "Name" };
-//            var hotel = new Hotel(){HotelId = 2, ExtraServices = new List<ExtraService>{ new ExtraService(), new ExtraService()}};
-//            _logger.LogDebug(hotel.ExtraServices.Count().ToString());
-//            var (list, MappedEntity) = Test.Test.TestMapping(dto, hotel, _mapper, _logger);
-//            _logger.LogDebug(MappedEntity.ExtraServices.Count().ToString());
-//            return string.Join("\n", list);
-            //return new string[] { "value1", "value2" };
         }
 
         // GET: api/Hotels/5
@@ -73,7 +54,7 @@ namespace BookingSystem.WebApi.Controllers
         /// <param name="id"></param>
         /// <returns></returns>
         [HttpGet("{id}")]
-        public async Task<IActionResult> GetHotel(int id)
+        public async Task<IActionResult> GetHotelAsync(int id)
         {
             var result = await _queryDispatcher.Dispatch(new HotelDetailsQuery(id));
             if (result == null)
@@ -90,13 +71,15 @@ namespace BookingSystem.WebApi.Controllers
         /// <response code="404">If city not found</response>
         /// <responce code="201">New hotel created</responce>
         [HttpPost]
-        [ProducesResponseType(201)]
-        [ProducesResponseType(404)]
-        public async Task<IActionResult> AddHotel([FromBody] NewHotelDto hotel)
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> AddHotelAsync([FromBody] NewHotelDto hotel)
         {
-            _logger.LogDebug("Start add");
-            var result = await _commandDispatcher.Dispatch(new AddHotelCommand(hotel));
-            return FromResult(result);
+            var result = await _commandDispatcher.DispatchAsync(new AddHotelCommand(hotel));
+            if (result.IsSuccessful == false)
+                return BadRequest(result);
+            return CreatedAtAction(nameof(GetHotelAsync), new {id = result.Value}, null);
+//            return FromResult(result);
         }
 
         // PUT: api/Hotels/5
@@ -112,9 +95,10 @@ namespace BookingSystem.WebApi.Controllers
             //Todo: ask
             if (id != hotel.HotelId)
                 return BadRequest();
-            var result = await _commandDispatcher.Dispatch(new EditHotelCommand(hotel));
-            return FromResult(result);
-            //return result.IsSuccessful ? CreatedAtAction(nameof(GetHotel), new { id = result.Value }) : BadRequest(result.ErrorMessage);
+            var result = await _commandDispatcher.DispatchAsync(new EditHotelCommand(hotel));
+            if (result.IsSuccessful == false)
+                return BadRequest(result);
+            return CreatedAtAction(nameof(GetHotelAsync), new { id = result.Value }, null);
         }
 
         // DELETE: api/Hotel/5
@@ -126,7 +110,7 @@ namespace BookingSystem.WebApi.Controllers
         [HttpDelete("{id}")]
         public async Task<IActionResult> DeleteHotel(int id)
         {
-            var result = await _commandDispatcher.Dispatch(new DeleteHotelCommand(id));
+            var result = await _commandDispatcher.DispatchAsync(new DeleteHotelCommand(id));
             return FromResult(result);
         }
     }
