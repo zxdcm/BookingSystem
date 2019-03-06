d4fef289ef392705c11cac1516fb784382a92719
Egor Kirilin <kirilin.egor@gmail.com>
2019-03-06 14:30:06 +0300

    BookingConfiguration class added.

---
+++ BookingSystem/BookingSystem.WebApi/Utils/BookingConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookingSystem.Common;
+using BookingSystem.Common.Interfaces;
+using Microsoft.Extensions.Configuration;
+
+namespace BookingSystem.WebApi.Utils
+{
+    public class BookingConfiguration : IBookingConfiguration
+    {
+        public int LockTimeOutMinutes => _configuration.GetValue("BookingRules:TimeOutMinutes", 30);
+
+        private readonly IConfiguration _configuration;
+
+        public BookingConfiguration(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }        
+    }
+}
