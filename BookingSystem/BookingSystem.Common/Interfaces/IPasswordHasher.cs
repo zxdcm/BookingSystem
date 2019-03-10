b6f1fa9b0f1a50c9d414bce8eb53d7633e110535
Egor Kirilin <z@xd.cm>
2019-03-10 20:50:46 +0300

    PasswordHasher added

---
+++ BookingSystem/BookingSystem.Common/Interfaces/IPasswordHasher.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BookingSystem.Common.Utils;
+using BookingSystem.WritePersistence.WriteModels;
+
+namespace BookingSystem.Common.Interfaces
+{
+    public interface IPasswordHasher
+    {
+        string HashPassword(string password);
+        bool VerifyPassword(string hash, string providedPassword);
+    } 
+
+}
---
+++ BookingSystem/BookingSystem.Common/Utils/PasswordHasher.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
+using System.Text;
+using BookingSystem.Common.Interfaces;
+using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+
+namespace BookingSystem.Common.Utils
+{
+    public class PasswordHasher : IPasswordHasher
+    {
+        private readonly int _iterCount = 1000;
+        private readonly RandomNumberGenerator _rng = new RNGCryptoServiceProvider();
+
+        public string HashPassword(string password)
+        {
+            return Convert.ToBase64String(HashPasswordV3(password));
+        }
+
+
+        public bool VerifyPassword(string hash, string providedPassword)
+        {
+            var decodedHashedPassword = Convert.FromBase64String(hash);
+            if (VerifyHashedPasswordV3(decodedHashedPassword, providedPassword))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        private byte[] HashPasswordV3(string password)
+        {
+            return HashPasswordV3(password, _rng,
+                prf: KeyDerivationPrf.HMACSHA256,
+                iterCount: _iterCount,
+                saltSize: 128 / 8,
+                numBytesRequested: 256 / 8);
+        }
+
+        private static void WriteNetworkByteOrder(byte[] buffer, int offset, uint value)
+        {
+            buffer[offset + 0] = (byte)(value >> 24);
+            buffer[offset + 1] = (byte)(value >> 16);
+            buffer[offset + 2] = (byte)(value >> 8);
+            buffer[offset + 3] = (byte)(value >> 0);
+        }
+
+        private static byte[] HashPasswordV3(string password, RandomNumberGenerator rng, KeyDerivationPrf prf, int iterCount, int saltSize, int numBytesRequested)
+        {
+            byte[] salt = new byte[saltSize];
+            rng.GetBytes(salt);
+            byte[] subkey = KeyDerivation.Pbkdf2(password, salt, prf, iterCount, numBytesRequested);
+
+            var outputBytes = new byte[13 + salt.Length + subkey.Length];
+            outputBytes[0] = 0x01; 
+            WriteNetworkByteOrder(outputBytes, 1, (uint)prf);
+            WriteNetworkByteOrder(outputBytes, 5, (uint)iterCount);
+            WriteNetworkByteOrder(outputBytes, 9, (uint)saltSize);
+            Buffer.BlockCopy(salt, 0, outputBytes, 13, salt.Length);
+            Buffer.BlockCopy(subkey, 0, outputBytes, 13 + saltSize, subkey.Length);
+            return outputBytes;
+        }
+
+        private static uint ReadNetworkByteOrder(byte[] buffer, int offset)
+        {
+            return ((uint)(buffer[offset + 0]) << 24)
+                   | ((uint)(buffer[offset + 1]) << 16)
+                   | ((uint)(buffer[offset + 2]) << 8)
+                   | ((uint)(buffer[offset + 3]));
+        }
+
+        private static bool VerifyHashedPasswordV3(byte[] hashedPassword, string password)
+        {
+            try
+            {
+
+                KeyDerivationPrf prf = (KeyDerivationPrf)ReadNetworkByteOrder(hashedPassword, 1);
+                int iterCount = (int)ReadNetworkByteOrder(hashedPassword, 5);
+                int saltLength = (int)ReadNetworkByteOrder(hashedPassword, 9);
+
+                // Read the salt: must be >= 128 bits
+                if (saltLength < 128 / 8)
+                {
+                    return false;
+                }
+                byte[] salt = new byte[saltLength];
+                Buffer.BlockCopy(hashedPassword, 13, salt, 0, salt.Length);
+
+                // Read the subkey (the rest of the payload): must be >= 128 bits
+                int subkeyLength = hashedPassword.Length - 13 - salt.Length;
+                if (subkeyLength < 128 / 8)
+                {
+                    return false;
+                }
+                byte[] expectedSubkey = new byte[subkeyLength];
+                Buffer.BlockCopy(hashedPassword, 13 + salt.Length, expectedSubkey, 0, expectedSubkey.Length);
+
+                // Hash the incoming password and verify it
+                byte[] actualSubkey = KeyDerivation.Pbkdf2(password, salt, prf, iterCount, subkeyLength);
+                return ByteArraysEqual(actualSubkey, expectedSubkey);
+            }
+            catch
+            {
+                // This should never occur except in the case of a malformed payload, where
+                // we might go off the end of the array. Regardless, a malformed payload
+                // implies verification failed.
+                return false;
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining | MethodImplOptions.NoOptimization)]
+        private static bool ByteArraysEqual(byte[] a, byte[] b)
+        {
+            if (a == null && b == null)
+            {
+                return true;
+            }
+            if (a == null || b == null || a.Length != b.Length)
+            {
+                return false;
+            }
+            var areSame = true;
+            for (var i = 0; i < a.Length; i++)
+            {
+                areSame &= (a[i] == b[i]);
+            }
+            return areSame;
+        }
+
+    }
+}
