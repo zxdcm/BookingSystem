1ceee4379b590534923cec4e051dcb16d69d4a5a
Egor Kirilin <z@xd.cm>
2019-03-12 09:44:11 +0300

    UserDetailsQuery added

---
+++ BookingSystem/BookingSystem.Queries/Queries/UserQueries/Queries/UserDetailsQuery.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BookingSystem.Common.Interfaces;
+using BookingSystem.Queries.Queries.UserQueries.Views;
+using BookingSystem.ReadPersistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookingSystem.Queries.Queries.UserQueries.Queries
+{
+    public class UserDetailsQuery : IQuery<UserView>
+    {
+        public int UserId { get; }
+
+        public UserDetailsQuery(int userId)
+        {
+            UserId = userId;
+        }
+    }
+
+    public class RoomDetailsQueryHandler : IQueryHandler<UserDetailsQuery, UserView>
+    {
+        private readonly BookingReadContext _dataContext;
+
+        public RoomDetailsQueryHandler(BookingReadContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+        public Task<UserView> ExecuteAsync(UserDetailsQuery query)
+        {
+            var users = from user in _dataContext.Users
+                where user.UserId == query.UserId
+                join userRoles in _dataContext.UserRoles on user.UserId equals userRoles.UserId
+                join role in _dataContext.Roles on userRoles.RoleId equals role.RoleId into roles
+                from role in roles.DefaultIfEmpty()
+                group role by new
+                {
+                    UserId = user.UserId,
+                    Email = user.Email,
+                    FirstName = user.FirstName,
+                    SecondName = user.SecondName,
+                }
+                into gr
+                select new UserView()
+                {
+                    UserId = gr.Key.UserId,
+                    Email = gr.Key.Email,
+                    FirstName = gr.Key.FirstName,
+                    SecondName = gr.Key.SecondName,
+                    Roles = gr.Select(r => r.Name),
+                };
+            return Task.FromResult(users.FirstOrDefault());
+        }
+    }
+}
---
+++ BookingSystem/BookingSystem.Queries/Queries/UserQueries/Views/UserView.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookingSystem.Queries.Queries.UserQueries.Views
+{
+    public class UserView
+    {
+        public int UserId { get; set; }
+        public string FirstName { get; set; }
+        public string SecondName { get; set; }
+        public string Email { get; set; }
+        public IEnumerable<string> Roles { get; set; }
+
+    }
+}
