f1955c4b458c81215632b32b75cfa3ca43955e44
Egor Kirilin <z@xd.cm>
2019-03-12 09:44:52 +0300

    User role assigning when user signup added.

---
+++ BookingSystem/BookingSystem.Common/Utils/RoleName.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookingSystem.Common.Utils
+{
+    public class RoleName
+    {
+        public static readonly string Admin = "Admin";
+        public static readonly string User = "User";
+        public static readonly string Manager = "Manager";
+    }
+}
